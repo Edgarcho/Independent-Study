ee69802757c52d9fcc0e36fe7ca1a542bcedef9b
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-26 11:59:02 -0800

    Testing different arguments in getDateInstance method

--- src/main/java/time.java
+++ src/main/java/time.java
@@ -1,3 +1,4 @@
+import javax.xml.crypto.Data;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -15,7 +16,19 @@ public class time {
         //Using DateFormat class
         Date now = new Date();
         DateFormat df = DateFormat.getDateInstance();
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.SHORT);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.LONG);
+        DateFormat df3 = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        DateFormat df4 = DateFormat.getDateInstance(DateFormat.AM_PM_FIELD);
         String s = df.format(now);
-        System.out.println("Today is" + s);
+        String s1 = df1.format(now);
+        String s2 = df2.format(now);
+        String s3 = df3.format(now);
+        String s4 = df4.format(now);
+        System.out.println("Today is " + s);
+        System.out.println("Today is " + s1);
+        System.out.println("Today is " + s2);
+        System.out.println("Today is " + s3);
+        System.out.println("Today is " + s4);
     }
 }
