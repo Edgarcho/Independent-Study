15c0258bccb74c3426f3a3dc7a2b1eb0dfcd7deb
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-26 13:53:56 -0800

    Created getter method for year in test

--- src/test/java/BirthdayTest.java
+++ src/test/java/BirthdayTest.java
@@ -7,8 +7,14 @@ import static org.junit.Assert.*;
  */
 public class BirthdayTest {
     @Test
-    public void newAge_instantiatesCorrectly() throws Exception {
+    public void newBirthday_instantiatesCorrectly() throws Exception {
         Birthday testBirthday = new Birthday(1992,3,6);
         assertEquals(true, testBirthday instanceof Birthday);
+    }
+
+    @Test
+    public void newBirthday_getYear_1992() throws Exception {
+        Birthday testBirthday = new Birthday(1992,3,6);
+        assertEquals(1992, testBirthday.getYear());
     }
 }
