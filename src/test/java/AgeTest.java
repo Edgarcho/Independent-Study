7a105e507a3b30fa909b78a6f9cf24693a097b7f
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-26 13:41:00 -0800

    Created first test to get instance of Age class

--- src/test/java/AgeTest.java
+++ src/test/java/AgeTest.java
@@ -1,8 +1,14 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Guest on 1/26/18.
  */
 public class AgeTest {
-
+    @Test
+    public void newAge_instantiatesCorrectly() throws Exception {
+        Age testAge = new Age(1992,3,6);
+        assertEquals(true, testAge instanceof Age);
+    }
 }
