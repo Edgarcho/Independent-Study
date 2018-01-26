ac0e334be55558d3a30bf7d1316e3e1e27e4f557
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-26 11:42:27 -0800

    Add data class into Time class

--- src/main/java/time.java
+++ src/main/java/time.java
@@ -1,5 +1,13 @@
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Created by Guest on 1/26/18.
  */
 public class time {
+    public static void main(String[] args) {
+        Date now = new Date();
+        long nowLong = now.getTime();
+        System.out.println("Value is " + nowLong);
+    }
 }
