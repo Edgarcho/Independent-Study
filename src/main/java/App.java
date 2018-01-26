a09afd4ea75819648e66528ff63a08bfa5e7992c
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-26 15:27:19 -0800

    Added front-end logic to App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import javax.imageio.IIOException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import model.Birthday;
 
 /**
  * Created by Guest on 1/26/18.
@@ -10,13 +11,23 @@ public class App {
     public static void main(String[] args) {
         try{
             BufferedReader bufferedReader =  new BufferedReader(new InputStreamReader(System.in));
-            String inputString = bufferedReader.readLine();
-            System.out.println(inputString);
+            System.out.println("What year were you born?");
+            String userYear = bufferedReader.readLine();
+            System.out.println("What month were you born?");
+            String userMonth = bufferedReader.readLine();
+            System.out.println("What day were you born?");
+            String userDay = bufferedReader.readLine();
+            int intUserYear = Integer.parseInt(userYear);
+            int intUserMonth = Integer.parseInt(userMonth);
+            int intUserDay = Integer.parseInt(userDay);
+            Birthday newBirthday = new Birthday(intUserYear,intUserMonth,intUserDay);
+            System.out.println(newBirthday.dateFullForm());
         }
         catch(IOException e)
         {
             e.printStackTrace();
         }
+
     }
 
 
