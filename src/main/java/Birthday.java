d76cd2ec95a67053616300bd55ac2bacd3fe6e05
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-26 13:56:43 -0800

    Test passed

--- src/main/java/Birthday.java
+++ src/main/java/Birthday.java
@@ -2,9 +2,12 @@
  * Created by Guest on 1/26/18.
  */
 public class Birthday {
+    private int year;
+
     public Birthday(int year, int month, int date) {
+        this.year = year;
     }
     public int getYear(){
-        return 0;
+        return year;
     }
 }
