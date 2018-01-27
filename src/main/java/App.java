a9b688457f98a4ddf98a2fabf30d587bbbb64ffd
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-26 16:03:13 -0800

    Add while loop in App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,26 +9,37 @@ import model.Birthday;
  */
 public class App {
     public static void main(String[] args) {
-        try{
-            BufferedReader bufferedReader =  new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("What year were you born?");
-            String userYear = bufferedReader.readLine();
-            System.out.println("What month were you born?");
-            String userMonth = bufferedReader.readLine();
-            System.out.println("What day were you born?");
-            String userDay = bufferedReader.readLine();
-            int intUserYear = Integer.parseInt(userYear);
-            int intUserMonth = Integer.parseInt(userMonth);
-            int intUserDay = Integer.parseInt(userDay);
-            Birthday newBirthday = new Birthday(intUserYear,intUserMonth,intUserDay);
-            System.out.println("You were born on " + newBirthday.dateFullForm());
-        }
-        catch(IOException e)
-        {
-            e.printStackTrace();
-        }
 
+        boolean programRunning = true;
+
+        while (programRunning) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Welcome to our program. Would you like to check it out? Enter: yes or no");
+            try {
+                String navigationChoice = bufferedReader.readLine();
+
+                if (navigationChoice.equals("yes")) {
+                    System.out.println("What year were you born?");
+                    String userYear = bufferedReader.readLine();
+                    System.out.println("What month were you born?");
+                    String userMonth = bufferedReader.readLine();
+                    System.out.println("What day were you born?");
+                    String userDay = bufferedReader.readLine();
+                    int intUserYear = Integer.parseInt(userYear);
+                    int intUserMonth = Integer.parseInt(userMonth);
+                    int intUserDay = Integer.parseInt(userDay);
+                    Birthday newBirthday = new Birthday(intUserYear, intUserMonth, intUserDay);
+                    System.out.println("You were born on " + newBirthday.dateFullForm());
+                } else if (navigationChoice.equals("no")) {
+                    System.out.println("Goodbye!");
+                    programRunning = false;
+                } else {
+                    System.out.println("I'm sorry, we don't recognize your input");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
-
-
 }
